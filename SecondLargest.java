c184acf1cfe811737dffe419e8fd7045a1065883
SaduniHettihewa <hhsnimasha98@gmail.com>
2021-06-06 13:10:13 +0530

    add

---
+++ SecondLargest.java
@@ -0,0 +1,27 @@
+package secondlargest;
+
+public class SecondLargest {
+
+    public static void main(String[] args) {
+
+        int largest = 0;
+        int secontLargest = 0;
+
+        int[] arry = {20,5, 8, 9, 18, 5, 20, 20, 8, 19, 19};
+
+        for (int i = 0; i < arry.length; i++) {
+            if (arry[i] > largest) {
+                secontLargest = largest;
+                largest = arry[i];
+            } else if (arry[i] == largest) {
+                largest = arry[i];
+
+            } else if (arry[i] > secontLargest) {
+                secontLargest = arry[i];
+            }
+        }
+        System.out.println("second large no is : " + secontLargest);
+        System.out.println("Largest no is :"+largest);
+    }
+
+}
